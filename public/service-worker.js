38a76183c0f681175c10b6495ca449d66ab1a6d7
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-13 10:52:29 -0700

    fixed typos

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,9 +2,9 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'verson 01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "./index.hteml",
+    "./index.html",
     "./js/index.js",
     "./js/idb/js",
     "./css/style.css",
     "./manifest.json"
-];l
+];
