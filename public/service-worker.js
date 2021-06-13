6be9de2f01fe0981963bf56055b77ad3848d2b72
EliptickBlock5 <nbjohnson@gmail.com>
2021-06-13 10:58:49 -0700

    added event listener to service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,3 +8,21 @@ const FILES_TO_CACHE = [
     "./css/style.css",
     "./manifest.json"
 ];
+
+//pulling items from cache as resp
+self.addEventListener('fetch', function(e) {
+    console.log('nate fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('Nate responding with cache : ' + e.request.url)
+                return request
+            }
+
+            else {
+                console.log('Nate file is not cached, fetching :' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
